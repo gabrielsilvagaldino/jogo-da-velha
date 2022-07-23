7f3ca3f552eba51b12f9c309cda0da1da1263b64
gabrielsilvagaldino <gabrielgaldino3004@gmail.com>
2022-07-23 12:23:06 -0300

    Adiciona alternador no grid, e o valor fixo

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const grid = document.querySelector('#grid');
 
-function criaGrid() {
+let selecionado = 'red'
+
+const criaGrid = () => {
     for (let i = 0; i < 9; i += 1) {
         let criaBloco = document.createElement('div')
         criaBloco.className = 'grid-block'
         grid.appendChild(criaBloco)
     }
 }
-
-function colocaCruz() {
-    
-}
-
 criaGrid();
+
+const gridFilho = document.querySelectorAll('.grid-block')
+let alternador = -1
+for (let i = 0; i < gridFilho.length; i += 1) {
+    gridFilho[i].addEventListener('click', () => {
+      if (alternador === -1 && gridFilho[i].innerHTML === '') {
+        gridFilho[i].className = 'grid-block ' + selecionado
+        gridFilho[i].innerHTML= 'X'
+        selecionado = 'blue'
+        alternador += 1
+      } else if (alternador === 0 && gridFilho[i].innerHTML === '') {
+          gridFilho[i].className = 'grid-block ' + selecionado
+          gridFilho[i].innerHTML= 'O'
+          selecionado = 'red'
+          alternador -= 1
+      }
+      console.log(alternador);
+    })
+}
