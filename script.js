41765f169aaf646991f65ef6104f47fe531122fd
gabrielsilvagaldino <gabrielgaldino3004@gmail.com>
2022-07-24 09:53:00 -0300

    Adicionado a primeira vitória para o vermelho

--- script.js
+++ script.js
@@ -13,19 +13,40 @@ criaGrid();
 
 const gridFilho = document.querySelectorAll('.grid-block')
 let alternador = -1
-for (let i = 0; i < gridFilho.length; i += 1) {
-    gridFilho[i].addEventListener('click', () => {
-      if (alternador === -1 && gridFilho[i].innerHTML === '') {
-        gridFilho[i].className = 'grid-block ' + selecionado
-        gridFilho[i].innerHTML= 'X'
-        selecionado = 'blue'
-        alternador += 1
-      } else if (alternador === 0 && gridFilho[i].innerHTML === '') {
+let array = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+const funcionalidadeDosBlocos = () => {
+  for (let i = 0; i < gridFilho.length; i += 1) {
+      gridFilho[i].addEventListener('click', () => {
+        if (alternador === -1 && gridFilho[i].innerHTML === '') {
           gridFilho[i].className = 'grid-block ' + selecionado
-          gridFilho[i].innerHTML= 'O'
-          selecionado = 'red'
-          alternador -= 1
-      }
-      console.log(alternador);
-    })
+          gridFilho[i].innerHTML= 'X'
+          array[i] = 1
+          console.log(array);
+          selecionado = 'blue'
+          alternador += 1
+        } else if (alternador === 0 && gridFilho[i].innerHTML === '') {
+            gridFilho[i].className = 'grid-block ' + selecionado
+            gridFilho[i].innerHTML= 'O'
+            array[i] = -1
+            console.log(array);
+            selecionado = 'red'
+            alternador -= 1
+        }
+        console.log(alternador);
+      })
+  }
 }
+funcionalidadeDosBlocos();
+
+
+
+const resultadoFinal = () => {
+    for (let i = 0; i < gridFilho.length; i += 1) {
+      gridFilho[i].addEventListener('click', () => {
+        if (array[0] + array[1] + array[2] === 3) setTimeout(alert ('VERMELHO GANHOU!!!'), 1000)
+    });
+  }
+}
+
+resultadoFinal();
