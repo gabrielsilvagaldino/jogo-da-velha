8d2f06211352deaa18eb83f05aa3939aaec5707f
gabrielsilvagaldino <gabrielgaldino3004@gmail.com>
2022-07-24 10:55:09 -0300

    Resultado de vitórias de azul foi adicionado

--- script.js
+++ script.js
@@ -44,15 +44,23 @@ funcionalidadeDosBlocos();
 const resultadoFinal = () => {
   for (let i = 0; i < gridFilho.length; i += 1) {
     gridFilho[i].addEventListener('click', () => {
+      // Vitória em horizontal
       for (let index = 0; index < gridFilho.length; index += 3) {
-        if (array[index] + (array[index+1]) + (array[index+2]) === 3) alert ('VERMELHO GANHOU!!!');
-      }
-      for (let count = 0; count < gridFilho.length; count += 1) {
-        if (array[count] + (array[count+3]) + (array[count+6]) === 3) alert ('VERMELHO GANHOU!!!');
+        if (array[index] + (array[index+1]) + (array[index+2]) === 3) alert ('VERMELHO GANHOU!!!') 
+        else if (array[index] + (array[index+1]) + (array[index+2]) === -3) alert ('AZUL GANHOU!!!') 
       }
 
+      // Vitória em vertical
+      for (let count = 0; count < gridFilho.length; count += 1) {
+        if (array[count] + (array[count+3]) + (array[count+6]) === 3) alert ('VERMELHO GANHOU!!!');
+        else if (array[count] + (array[count+3]) + (array[count+6]) === -3) alert ('AZUL GANHOU!!!');
+      }
+
+      // Vitória em X
       if (array[0] + array[4] + array[8] === 3) setTimeout(alert ('VERMELHO GANHOU!!!'), 1000)
+      else if (array[0] + array[4] + array[8] === -3) setTimeout(alert ('AZUL GANHOU!!!'), 1000)
       if (array[2] + array[4] + array[6] === 3) setTimeout(alert ('VERMELHO GANHOU!!!'), 1000)
+      else if (array[2] + array[4] + array[6] === -3) setTimeout(alert ('AZUL GANHOU!!!'), 1000)
     });
   }
 }
